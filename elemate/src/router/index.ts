bd6030a8eed6a6de99e90e33038886aa8c6877bf
Swixos <erazion.swixo@gmail.com>
2024-01-27 01:54:12 +0100

    ✏️ Suppression de la route AboutView car fichier supprimé

--- elemate/src/router/index.ts
+++ elemate/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../pages/HomeView.vue'
-import About from '../pages/AboutView.vue'
 import ElemateAnimals from '../pages/Animals.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -8,14 +7,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: About
   },
   {
     path: '/animals',
